448daaed3ec80b9586fdf075a6c0139e8ee2a9af
leoadvance <leo_advance@163.com>
2017-11-05 23:44:14 +0800

    学习import 类

--- HelloWorld/src/leoPrivate/Main.java
+++ HelloWorld/src/leoPrivate/Main.java
@@ -1,10 +1,11 @@
 package leoPrivate;
-
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 
-        System.out.print("Hello World");
+        System.out.print("Hello World\n");
+        Veriable veriable1 = new Veriable("1");
+        veriable1.Test();
     }
 }
---
+++ HelloWorld/src/leoPrivate/Veriable.java
@@ -0,0 +1,10 @@
+package leoPrivate;
+
+public class Veriable {
+    public void Test(){
+        System.out.print("变量测试！");
+    }
+    public Veriable(String name){
+        // 这个构造器仅有一个参数：name
+    }
+}
